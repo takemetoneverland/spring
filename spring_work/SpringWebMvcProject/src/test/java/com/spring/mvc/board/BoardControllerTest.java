63ffaaebd996f9d8fba27604710b2aa9eb7c70fb
Daeun Jung <122bittersweet@gmail.com>
2022-02-03 17:14:52 +0900

    2022.02.03

---
+++ spring_work/SpringWebMvcProject/src/test/java/com/spring/mvc/board/BoardControllerTest.java
@@ -0,0 +1,106 @@
+package com.spring.mvc.board;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import lombok.extern.log4j.Log4j;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = {"file:src/main/webapp/WEB-INF/spring/servlet-config.xml",
+"file:src/main/webapp/WEB-INF/spring/mvc-config.xml"})
+@WebAppConfiguration
+@Log4j //log를 찍는 도구. 따로 파일로 받을 수 있고 정보를 넣을 수 있다.
+public class BoardControllerTest {
+
+	//테스트 환경에서 가상의 DispatcherServlet을 사용하기 위한 객체 자동 주입.
+	//WebApplicationContext는 Spring에서 제공되는 servlet들을 사용할 수 있도록
+	//정보를 저장하는 객체이다.
+	@Autowired
+	private WebApplicationContext ctx;
+
+	//	@Autowired
+	//	private BoardController controller;
+
+	//MockMvc는 웹 어플리케이션을 서버에 배포하지 않아도 스프링 MVC 동작을
+	//구현할 수 있는 가상의 구조를 만들어 준다.
+	private MockMvc mockMvc;
+
+	@Before //test module에서는 junit의 before를 import test메서드가 실행되기 전에 미리 동작
+	public void setUp() {
+		//가상 구조를 세팅
+		//스프링 컨테이너에 등록된 모든 빈과 의존성 주입까지 로드해서 사용이 가능.
+		this.mockMvc = MockMvcBuilders.webAppContextSetup(ctx).build();
+
+		//테스트할 컨트롤러를 수동으로 주입. 하나의 컨트롤러만 테스트를 진행할 때 이렇게 쓴다.
+		//		this.mockMvc = MockMvcBuilders.standaloneSetup(controller).build();
+	}
+
+	@Test
+	public void testList() throws Exception { //예외를 던져줌
+		log.info(
+				mockMvc.perform(MockMvcRequestBuilders.get("/board/list"))
+				.andReturn()
+				.getModelAndView()
+				.getModelMap()
+				); //console창에 출력. setviewname(): 목적지
+	}
+
+	@Test
+	public void testInsert() throws Exception {
+		String viewPage = mockMvc.perform(MockMvcRequestBuilders.post("/board/write")
+											.param("title", "테스트 새 글 제목")
+											.param("content", "테스트 새 글 내용")
+											.param("writer", "user01")
+											).andReturn().getModelAndView().getViewName();
+		log.info(viewPage);
+	}
+
+	//게시글 상세보기 요청 넣어보기.
+	//컨트롤러에서는 DB에서 가지고 온 글 객체를 model에 담아서 jsp로 이동시킬 것입니다.
+	//42번글을 보여달라는 요청을 넣으시고, 요청 결과가 들어있는 model을 출력해 보세요.
+	// /board/content -> get
+	@Test
+	public void testContent() throws Exception {
+		log.info(mockMvc.perform(MockMvcRequestBuilders.get("/board/content")
+						.param("boardNo", "42")
+						).andReturn().getModelAndView().getModelMap()
+				);
+	}
+
+	//5번글의 제목과 내용을 수정하는 요청을 보낼 예정입니다.
+	//전송방식은 post 방식입니다.
+	//수정 후 이동하는 페이지는 해당 글의 상세보기 페이지입니다.
+	//컨트롤러가 리턴하는 viewName을 출력해 주세요. ("/board/modify") 제목과 내용만. 작성자는 수정x
+	@Test
+	public void testModify() throws Exception {
+		String viewName = mockMvc.perform(MockMvcRequestBuilders.post("/board/modify")
+				.param("title", "수정된 테스트 글 제목2")
+				.param("content", "수정된 테스트 글 내용 2")
+				.param("boardNo", "5")
+				).andReturn().getModelAndView().getViewName();
+		log.info(viewName);
+	}
+
+	//42번글을 삭제하세요.
+	//전송 방식은 post방식이고, 이동하는 곳은 목록 요청이 재요청될 것입니다.
+	//viewName을 출력해 주세요.
+	@Test
+	public void testRemove() throws Exception {
+		log.info(
+				mockMvc.perform(MockMvcRequestBuilders.post("/board/delete")
+						.param("boardNo", "42")
+						).andReturn().getModelAndView().getViewName()
+				);
+		
+	}
+	
+}
---
+++ spring_work/SpringWebMvcProject/src/test/java/com/spring/mvc/board/BoardMapperTest.java
@@ -0,0 +1,79 @@
+package com.spring.mvc.board;
+
+import java.util.List;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import com.spring.mvc.board.model.BoardVO;
+import com.spring.mvc.board.repository.IBoardMapper;
+
+@RunWith(SpringJUnit4ClassRunner.class) //test 가상환경을 만들어주는 객체
+@ContextConfiguration(locations = {"file:src/main/webapp/WEB-INF/spring/mvc-config.xml"})
+//mapper가 잘 작동되는지 mvc-config.xml에 있는 설정들 가져오겠다. 이게 없으면 주입이 안됨. main이 아니라서 자동 적용이 안된다.
+public class BoardMapperTest {
+
+	@Autowired //mvc-config.xml을 끌어오기 때문에 Autowired가 가능함
+	private IBoardMapper mapper; //IBoardMapper 인터페이스 타입의 객체가 필요함
+	
+	//게시글 등록 단위 테스트
+	@Test
+	public void insertTest() {
+		for(int i=1; i<=300; i++) {
+			BoardVO article = new BoardVO();
+			article.setTitle("테스트 제목입니다." + i);
+			article.setWriter("김테스트" + i);
+			article.setContent("테스트 중이니까 조용히 하세요!" + i);
+			mapper.insert(article);
+		}
+	}
+	
+	//게시글 목록 전체 조회 테스트
+	//게시물 갯수 몇개인지 출력하고, 게시글 모든 내용을 toString()으로 출력하세요.
+	@Test
+	public void getListTest() {
+		List<BoardVO> list = mapper.getArticleList();
+		System.out.println(list.size());
+		for(BoardVO vo : list) {
+			System.out.println(vo); //.toString 붙이지 않아도 자동으로 됨. lombok
+		}		
+	}
+
+	//게시물 단일 조회 테스트
+	//44번글을 조회해서 글 상세 내용을 출력해 주세요.
+	@Test
+	public void getArticleTest() {
+		BoardVO vo = mapper.getArticle(44);
+		System.out.println(vo.getContent());
+	}
+	
+	//게시글 수정 테스트
+	//BoardVO 객체를 하나 생성하고, VO의 setter를 사용하여
+	//수정 내용(글 제목, 글 내용)을 입력하고 수정을 진행해 보세요. (1번글의 제목, 내용)
+	@Test
+	public void updateTest() {
+		BoardVO vo = new BoardVO();
+		vo.setBoardNo(1);
+		vo.setTitle("수정될 제목 수정수정");
+		vo.setContent("수정입니다. 수정이에요. 내용 수정수정");
+		mapper.update(vo);
+		System.out.println("수정 후 정보: " + mapper.getArticle(1));
+	}
+	
+	//게시글 삭제 테스트
+	//3번글을 삭제하세요. 삭제 후 상세보기를 통해 3번글을 가져왔을 때 null이 리턴되는지
+	//조건문으로 확인해서 결과를 출력하세요. (null이 왔다는 것 -> 삭제 성공! 게시글 없음!)
+	@Test
+	public void deleteTest() {
+		mapper.delete(3);
+		if(mapper.getArticle(3) == null) { //조회 결과가 없으면 0이 온다.(myBatis)
+			System.out.println("삭제 성공! 게시글 없음!");
+		} else {
+			System.out.println("삭제 실패 ㅠㅠ");
+		}
+	}
+	
+}
